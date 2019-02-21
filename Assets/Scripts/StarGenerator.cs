8ed8c7dc70a7277d95ccca29333e5ce6dc29c368
Kevin Escobar Rodriguez <kevinaer@mit.edu>
2019-02-20 23:51:52 -0500

    Fixed perspective of planets and added side bounce to platforms

--- Assets/Scripts/StarGenerator.cs
+++ Assets/Scripts/StarGenerator.cs
@@ -7,13 +7,14 @@ public class StarGenerator : MonoBehaviour {
     public GameObject starPrefab;
     public int amount = 1;
     public float limitOffScreen = 0.25f;
+    public float maxVerticalDistance = 3f;
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < amount; i++)
         {
             Vector3 position = new Vector3(
                 Random.Range(0f, 1f) + limitOffScreen * Random.Range(-1f, 1f),
-                Random.Range(-3f, 3f), 
+                Random.Range(-maxVerticalDistance, 1f+maxVerticalDistance), 
                 10f);
             GameObject star = Instantiate(
                 starPrefab,
